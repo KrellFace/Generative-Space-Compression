4fb33593e1575520bf0246c58386201a98d61e14
KrellFace <owithington@hotmail.co.uk>
2022-01-17 17:13:09 +0000

    TSNE WIP

--- LodeRunnerPCA.py
+++ LodeRunnerPCA.py
@@ -6,6 +6,7 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 from sklearn.decomposition import TruncatedSVD
+from sklearn.manifold import TSNE
 from scipy.sparse import csr_matrix
 import matplotlib.pyplot as plt
 import prince
@@ -243,6 +244,46 @@ def generate_2dmatrix_col_names(height, width):
 def get_filenames_from_folder(path):
     return glob.glob(path + "*.txt")
 
+def return_coord_dict_fromcoord_lists(names, xcoords, ycoords):
+    output_dict = dict()
+    for v in range(0, len(names)):
+        output_dict[v] = [names[v],xcoords[v], ycoords[v]]
+    return output_dict
+
+
+#Get most extreme x and y values 
+def get_extreme_coords(level_coord_dict, extremecount = 5):
+    output_dict = dict()
+    #Get largest x
+    for x in range (0, extremecount):
+        currmaxkey = max(level_coord_dict, key = lambda k: level_coord_dict[k][1])
+        output_dict[currmaxkey] = level_coord_dict[currmaxkey]
+        level_coord_dict.pop(currmaxkey)
+    #Get largest y
+    for x in range (0, extremecount):
+        currmaxkey = max(level_coord_dict, key = lambda k: level_coord_dict[k][2])
+        output_dict[currmaxkey] = level_coord_dict[currmaxkey]
+        level_coord_dict.pop(currmaxkey)
+    #Get smallest x
+    for x in range (0, extremecount):
+        currminkey = min(level_coord_dict, key = lambda k: level_coord_dict[k][1])
+        output_dict[currminkey] = level_coord_dict[currminkey]
+        level_coord_dict.pop(currminkey)
+    #Get smallest y
+    for x in range (0, extremecount):
+        currminkey = min(level_coord_dict, key = lambda k: level_coord_dict[k][2])
+        output_dict[currminkey] = level_coord_dict[currminkey]
+        level_coord_dict.pop(currminkey)
+    
+    return output_dict
+
+
+def gen_component_labels_for_n(label, n):
+    output_labels = list()
+    for x in range (1, n+1):
+        output_labels.append(label + str(x))
+    return output_labels
+
 ###################################
 #Wrapper Methods
 
@@ -258,6 +299,7 @@ def get_all_one_hot_boxoban_from_file(path, tile_dict):
      
     level_dict = get_boxoban_leveldict_from_file(path)
     return get_compiled_onehot_from_leveldict(level_dict, tile_dict, 10, 10)
+
 
 #########################
 #Graphing and Visualisation Methods
@@ -291,8 +333,14 @@ def plot_pca(pca_info, gen_names=[]):
                     #, c = color
                     , s = 50)       
 
-    for index, row in pca_info_for_each_level.iterrows():
-        ax.annotate(row['level_name'],(row['PC 1'],row['PC 2']))
+    #Get the most outlying values to label
+    coord_dict = return_coord_dict_fromcoord_lists(pca_info_for_each_level['level_name'].tolist(), pca_info_for_each_level['PC 1'].tolist(), pca_info_for_each_level['PC 2'].tolist())
+    extreme_coords_for_labeling = get_extreme_coords(coord_dict, 40)
+
+    for key in extreme_coords_for_labeling:
+        ax.annotate(extreme_coords_for_labeling[key][0], (extreme_coords_for_labeling[key][1],extreme_coords_for_labeling[key][2] ))
+    #for index, row in pca_info_for_each_level.iterrows():
+    #    ax.annotate(row['level_name'],(row['PC 1'],row['PC 2']))
 
     ax.legend(gen_names)
     ax.grid()
@@ -327,8 +375,14 @@ def plot_mca(mca_info, gen_names=[]):
                     #, c = color
                     , s = 50)       
 
-    for index, row in mca_info_for_each_level.iterrows():
-        ax.annotate(row['level_name'],(row['MCA-PC1'],row['MCA-PC2']))
+    coord_dict = return_coord_dict_fromcoord_lists(mca_info_for_each_level['level_name'].tolist(), mca_info_for_each_level['MCA-PC1'].tolist(), mca_info_for_each_level['MCA-PC2'].tolist())
+    extreme_coords_for_labeling = get_extreme_coords(coord_dict)
+
+    for key in extreme_coords_for_labeling:
+        ax.annotate(extreme_coords_for_labeling[key][0], (extreme_coords_for_labeling[key][1],extreme_coords_for_labeling[key][2]))
+
+    #for index, row in mca_info_for_each_level.iterrows():
+    #    ax.annotate(row['level_name'],(row['MCA-PC1'],row['MCA-PC2']))
 
     ax.legend(gen_names)
     ax.grid()
@@ -336,6 +390,7 @@ def plot_mca(mca_info, gen_names=[]):
 
 def plot_svd(toplot, gen_names = []):
     svdinfo = toplot[1]
+    #svd_var_exp_ratio = svdinfo.explained_variance_ratio_
     svd_var_exp_ratio = svdinfo.explained_variance_ratio_
     svd_projected_on_levels = toplot[0]
     print("Variance explained of plotted SVD")
@@ -372,25 +427,63 @@ def plot_svd(toplot, gen_names = []):
     ax.grid()
     plt.show()
 
+def plot_tsne(toplot, gen_names = []):
+    tsneinfo = toplot[1]
+    #svd_var_exp_ratio = svdinfo.explained_variance_ratio_
+    #svd_var_exp_ratio = svdinfo.explained_variance_ratio_
+    tsne_projected_on_levels = toplot[0]
+    #print("Variance explained of plotted SVD")
+    #print(svd_var_exp_ratio)
+    fig = plt.figure(figsize = (8,8))
+    ax = fig.add_subplot(1,1,1) 
+    ax.set_xlabel('TSNE 1') #+ str("{0:.3%}".format(svd_var_exp_ratio[0])), fontsize = 15)
+    ax.set_ylabel('TSNE 2') #+ str("{0:.3%}".format(svd_var_exp_ratio[1])), fontsize = 15)
+    ax.set_title('2 Component TSNE ', fontsize = 20)
 
+    #Color each generators points differently if we are running for multiple alternatives
+    if len(gen_names)>0:
+        for generator in gen_names:
+            #Generate a random color for the generator
+            rgb = np.random.rand(3,)
+            #Limit our targets to just current generator
+            to_keep = tsne_projected_on_levels['Generator_Name'] == generator
+            ax.scatter(tsne_projected_on_levels.loc[to_keep, 'TSNE 1']
+                        , tsne_projected_on_levels.loc[to_keep, 'TSNE 2']
+                        , c = [rgb]
+                        , s = 50)
+    #For single generator SVD
+    else:
+        ax.scatter(tsne_projected_on_levels['TSNE 1']
+                    , tsne_projected_on_levels['TSNE 2']
+                    #, c = color
+                    , s = 50)       
+
+    for index, row in tsne_projected_on_levels.iterrows():
+        #print(row['c1'], row['c2'])
+        ax.annotate(row['level_name'],(row['TSNE 1'],row['TSNE 2']))
+
+    ax.legend(gen_names)
+    ax.grid()
+    plt.show()
 
 ##################################
 #Compression Algorithm Methods
 
-def get_PC_values_from_compiled_onehot(onehot_input):
+def get_PC_values_from_compiled_onehot(onehot_input, conponent_count = 2):
     #Skip first column as thats the levelname column
     features = onehot_input.columns[1:]
     levelnames = onehot_input['level_name'].tolist()
 
     x = onehot_input.loc[:,features].values
     x = StandardScaler().fit_transform(x)
-    pca = PCA(n_components=2)
+    pca = PCA(n_components=conponent_count)
     components = pca.fit_transform(x)
 
     print("Reprojected PCA shape: " + str(components.shape))
+    pc_labels = gen_component_labels_for_n("PC ", conponent_count)
 
     principalDf = pd.DataFrame(data = components
-                , columns = ['PC 1', 'PC 2'])
+                , columns = pc_labels)
 
     principalDf['level_name'] = levelnames       
     
@@ -400,11 +493,11 @@ def get_PC_values_from_compiled_onehot(onehot_input):
     }
     return (principalDf,pc_var_explained)
 
-def get_mca_values_from_compiled_char_reps(charrep_input):
+def get_mca_values_from_compiled_char_reps(charrep_input, conponent_count = 2):
     levelnames = charrep_input['level_name'].tolist()
     levelrep_columns = charrep_input.drop(['level_name'], axis = 1)
 
-    mca = prince.MCA()
+    mca = prince.MCA(n_components=conponent_count)
 
     mca.fit(levelrep_columns)
     levels_mcaprojected = mca.fit_transform(levelrep_columns)
@@ -413,112 +506,157 @@ def get_mca_values_from_compiled_char_reps(charrep_input):
 
     truncmcaDF = pd.DataFrame()
 
-    truncmcaDF['MCA-PC1'] = levels_mcaprojected[0]
-    truncmcaDF['MCA-PC2'] = levels_mcaprojected[1]
+    for x in range(0, conponent_count):
+        truncmcaDF[('MCA-PC' + str(x))] = levels_mcaprojected[x]
+    #truncmcaDF['MCA-PC1'] = levels_mcaprojected[0]
+    #truncmcaDF['MCA-PC2'] = levels_mcaprojected[1]
     truncmcaDF['level_name'] = levelnames
     return (truncmcaDF, mca)
 
 
-def get_projected_truc_svd_values_from_compiled_onehot(onehot_input):
+def get_projected_truc_svd_values_from_compiled_onehot(onehot_input, conponent_count = 2):
     levelnames = onehot_input['level_name'].tolist()
     levelrep_columns = onehot_input.drop(['level_name'], axis = 1)
+    #print("Level rep head: ")
+    #print(levelrep_columns.head())
 
-    svd = TruncatedSVD(n_components=2, n_iter=7, random_state=42)
+    svd = TruncatedSVD(n_components=conponent_count, n_iter=7, random_state=42)
+
+    #We can centre the data first, making it equivalent
+    #sc = StandardScaler()
+    #levelrep_columns = sc.fit_transform(levelrep_columns)
 
     svd.fit(levelrep_columns)
+    print("SVD un transformed explained variance " + str(svd.explained_variance_))
     levels_svdprojected = svd.fit_transform(levelrep_columns)
 
     print("Reprojected shape: " + str(levels_svdprojected.shape))
 
     truncsvdDF = pd.DataFrame()
-    truncsvdDF['SVD 1'] = levels_svdprojected[:,0]
-    truncsvdDF['SVD 2'] = levels_svdprojected[:,1]
+
+    for x in range(0, conponent_count):
+        truncsvdDF[('SVD ' + str(x+1))] = levels_svdprojected[:,x]
+
+    #truncsvdDF['SVD 1'] = levels_svdprojected[:,0]
+    #truncsvdDF['SVD 2'] = levels_svdprojected[:,1]
     truncsvdDF['level_name'] = levelnames
     return (truncsvdDF, svd)
 
+def get_tsne_projection_from_onehot(onehot_input, conponent_count = 2):
+    levelnames = onehot_input['level_name'].tolist()
+    levelrep_columns = onehot_input.drop(['level_name'], axis = 1)
+
+    tsne = TSNE(n_components=conponent_count, n_iter=250, random_state=42)
+
+    tsne.fit(levelrep_columns)
+    levels_tsneprojected = tsne.fit_transform(levelrep_columns)
+
+    print("Reprojected shape: " + str(levels_tsneprojected.shape))
+
+
+    trunctsneDF = pd.DataFrame()
+    for x in range(0, conponent_count):
+        trunctsneDF[('TSNE ' + str(x+1))] = levels_tsneprojected[:,x]
+    #trunctsneDF['TSNE 1'] = levels_tsneprojected[:,0]
+    #trunctsneDF['TSNE 2'] = levels_tsneprojected[:,1]
+    trunctsneDF['level_name'] = levelnames
+    return (trunctsneDF, tsne)
+
+################################
+#MULTIGENERATOR METHODS
+
 #Retrieve multiple folders worth of levels from different generators to simultaneously analyse
 #Runs Principal Component Analysis on onehot matrix versions of game levels
-def multigenerator_pca_analysis(generator_levels_dict,tile_dict,height, width):
+def multigenerator_pca_analysis(generator_levels_dict,tile_dict,height, width, component_count = 2):
     
     #Storage for our compiled dataframe from all folders
     output_dfs = []
-    #generator_names =[]
     for generator in generator_levels_dict:
         #Get all one for for specific folder
-        #current_set = get_all_onehot_from_folder_size_specified(folders_dict[folder], tile_dict,  height, width)
         level_dict = generator_levels_dict[generator]
         compiled_onehot_set = get_compiled_onehot_from_leveldict(level_dict, tile_dict, height, width)
-        #print("Curr set type: " + current_set.dtype)
         compiled_onehot_set['Generator_Name'] =  generator
-        #print("Curr set type: " + current_set.dtype)
-        #generator_names.append(generator)
         output_dfs.append(compiled_onehot_set)
     all_levels_onehot =  pd.concat(output_dfs, ignore_index=False)
 
     gen_name_list = all_levels_onehot['Generator_Name'].tolist()
-    pca_analysis = get_PC_values_from_compiled_onehot(all_levels_onehot.drop('Generator_Name', axis=1))
+    pca_analysis = get_PC_values_from_compiled_onehot(all_levels_onehot.drop('Generator_Name', axis=1), component_count)
     
     #Readding the name of the generator for each level to the list of all levels and their PCs
     pca_analysis[0]['Generator_Name'] = gen_name_list
-
-    #plot_pca_with_generator_name([pca_df_with_generator_names,pc_variance_explained], generator_names)
     plot_pca(pca_analysis, list(generator_levels_dict.keys()))
+    return pca_analysis[0]
 
 
 #Runs singular value decomposition on onehot representations of game levels
-def multigenerator_svd(generator_levels_dict,tile_dict,height, width):
+def multigenerator_svd(generator_levels_dict,tile_dict,height, width, component_count = 2):
     
     #Storage for our compiled dataframe from all folders
     output_dfs = []
-    #generator_names =[]
     for generator in generator_levels_dict:
         #Get all one for for specific folder
         level_dict = generator_levels_dict[generator]
         compiled_onehot_set = get_compiled_onehot_from_leveldict(level_dict, tile_dict, height, width)
-        #print("Curr set type: " + current_set.dtype)
         compiled_onehot_set['Generator_Name'] =  generator
-        #print("Curr set type: " + current_set.dtype)
-        #generator_names.append(generator)
         output_dfs.append(compiled_onehot_set)
     all_levels_onehot =  pd.concat(output_dfs, ignore_index=False)
 
     gen_name_list = all_levels_onehot['Generator_Name'].tolist()
 
-    svd_info = get_projected_truc_svd_values_from_compiled_onehot(all_levels_onehot.drop('Generator_Name', axis=1))
+    svd_info = get_projected_truc_svd_values_from_compiled_onehot(all_levels_onehot.drop('Generator_Name', axis=1), component_count)
     #Readding the name of the generator for each level to the list of all levels and their PCs
     svd_info[0]['Generator_Name'] = gen_name_list
     plot_svd(svd_info, list(generator_levels_dict.keys()))
 
-
-def multigenerator_mca(generator_levels_dict, height, width):
+def multigenerator_tsne(generator_levels_dict,tile_dict,height, width):
+    
     #Storage for our compiled dataframe from all folders
     output_dfs = []
-    #generator_names =[]
     for generator in generator_levels_dict:
         #Get all one for for specific folder
         level_dict = generator_levels_dict[generator]
+        compiled_onehot_set = get_compiled_onehot_from_leveldict(level_dict, tile_dict, height, width)
+        compiled_onehot_set['Generator_Name'] =  generator
+        output_dfs.append(compiled_onehot_set)
+    all_levels_onehot =  pd.concat(output_dfs, ignore_index=False)
+
+    gen_name_list = all_levels_onehot['Generator_Name'].tolist()
+
+    tsne_info = get_tsne_projection_from_onehot(all_levels_onehot.drop('Generator_Name', axis=1))
+    #Readding the name of the generator for each level to the list of all levels and their PCs
+    tsne_info[0]['Generator_Name'] = gen_name_list
+    #print('TSNE KL divergence : ' + str(tsne_info[1].kl_divergence_))
+    plot_tsne(tsne_info, list(generator_levels_dict.keys()))
+
+def multigenerator_mca(generator_levels_dict, height, width, conponent_count = 2):
+    #Storage for our compiled dataframe from all folders
+    output_dfs = []
+    for generator in generator_levels_dict:
+        level_dict = generator_levels_dict[generator]
         current_set = get_compiled_char_representations_from_level_dict(level_dict, height, width)
-        #print("Curr set type: " + current_set.dtype)
         current_set['Generator_Name'] =  generator
-        #print("Curr set type: " + current_set.dtype)
-        #generator_names.append(generator)
         output_dfs.append(current_set)
     all_levels_char_df =  pd.concat(output_dfs, ignore_index=False)
 
     gen_name_list = all_levels_char_df['Generator_Name'].tolist()
-    #only_cell_columns = all_levels_char_df.drop(['level_name','Generator_Name'], axis = 1)
 
-    mca_info = get_mca_values_from_compiled_char_reps(all_levels_char_df.drop('Generator_Name', axis=1))
+    mca_info = get_mca_values_from_compiled_char_reps(all_levels_char_df.drop('Generator_Name', axis=1), conponent_count)
     #Readding the name of the generator for each level to the list of all levels and their PCs
     mca_info[0]['Generator_Name'] = gen_name_list
 
-    #mca = prince.MCA()
-    #mca = mca.fit(only_cell_columns)
-    #print("MCA Explained interia:")
-    #print(mca_info[1].explained_inertia_)
     plot_mca(mca_info, list(generator_levels_dict.keys()))
 
+#Testing TSNE on output from others
+boxoban_file_levels_dict = get_nested_generator_level_dictionaries_from_boxoban_files(boxoban_files_dict, 10, 10)
+pca50 = multigenerator_pca_analysis(boxoban_file_levels_dict, boxoban_tiletypes_dict, 10, 10, 10)
+gen_name_list = pca50['Generator_Name'].tolist()
+tsneinfo = get_tsne_projection_from_onehot(pca50.drop('Generator_Name', axis=1))
+tsneinfo[0]['Generator_Name'] = gen_name_list
+plot_tsne(tsneinfo, list(boxoban_files_dict.keys()))
 
+#Testing TSNE
+#boxoban_file_levels_dict = get_nested_generator_level_dictionaries_from_boxoban_files(boxoban_files_dict, 10, 10)
+#multigenerator_tsne(boxoban_file_levels_dict, boxoban_tiletypes_dict, 10, 10)
 
 #Testing All methods on Mario
 #generator_levels_dict = get_nested_generator_level_dictionaries_from_folders(mario_folders_dict, 10, 80)
@@ -527,15 +665,17 @@ def multigenerator_mca(generator_levels_dict, height, width):
 #multigenerator_mca(generator_levels_dict,10,80)
 
 #Testing ALL methods on Boxoban
-#boxoban_file_levels_dict = get_nested_generator_level_dictionaries_from_boxoban_files(boxoban_files_dict, 10, 10)
-#multigenerator_pca_analysis(boxoban_file_levels_dict, boxoban_tiletypes_dict, 10, 10)
-#multigenerator_svd(boxoban_file_levels_dict, boxoban_tiletypes_dict, 10, 10)
-#multigenerator_mca(boxoban_file_levels_dict,10,10)
+boxoban_file_levels_dict = get_nested_generator_level_dictionaries_from_boxoban_files(boxoban_files_dict, 10, 10)
+#multigenerator_svd(boxoban_file_levels_dict, boxoban_tiletypes_dict, 10, 10, 10)
+#multigenerator_mca(boxoban_file_levels_dict,10,10, 5)
+#multigenerator_tsne(boxoban_file_levels_dict, boxoban_tiletypes_dict, 10,10)
+
+
 
 #Testing all methods on Loderunner
-lr_levels_dict = get_leveldict_from_individual_files_in_folder_size_specified(loderunnder_path, 22, 32)
-test_dict = dict()
-test_dict['Default'] = lr_levels_dict
-multigenerator_pca_analysis(test_dict, lr_tiletypes_dict,  22, 32)
-multigenerator_svd(test_dict, lr_tiletypes_dict,  22, 32)
-multigenerator_mca(test_dict, 22, 32)
+#lr_levels_dict = get_leveldict_from_individual_files_in_folder_size_specified(loderunnder_path, 22, 32)
+#test_dict = dict()
+#test_dict['Default'] = lr_levels_dict
+#multigenerator_pca_analysis(test_dict, lr_tiletypes_dict,  22, 32)
+#multigenerator_svd(test_dict, lr_tiletypes_dict,  22, 32)
+#multigenerator_mca(test_dict, 22, 32)
